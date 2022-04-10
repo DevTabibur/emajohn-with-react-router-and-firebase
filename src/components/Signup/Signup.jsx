a8d1b006c04d1ebd12d4f0ceb55332edb7e3b498
DevTabibur <tobiburrohman2@gmail.com>
2022-04-10 23:25:37 +0600

    sign up compo done

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Signup.css';
 
 const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleEmaliBlur = (e) =>{
+        console.log(e.target.email.value)
+    }
+
     return (
-        <div>
-        <h2>This i signup page</h2>
-            
-        </div>
+        <div className="form-container">
+      <div>
+        <h2 className="form-title">Sign Up</h2>
+        <form action="">
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input onClick={handleEmaliBlur} type="email" name="email" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Confirm Password</label>
+            <input type="confirm-password" name="confirm-password" id="" required />
+          </div>
+          <input className="form-submit" type="submit" value="Sign Up" />
+        </form>
+        <p>
+        Already have an account ? 
+        <Link className="form-link" to="/login"> Login</Link>
+        </p>
+      </div>
+    </div>
     );
 };
 
